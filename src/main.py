cb3c11cac79d86e8d5191cb82f96a6ea84fe6a95
rameshkpit <151899670+rameshkpit@users.noreply.github.com>
2023-12-15 18:23:33 +0530

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -4,6 +4,11 @@ def multiply_numbers(num1, num2):
     return num1 + num2
  
 if __name__ == "__main__":
+
+    if len(sys.argv) < 3:
+        print("Usage: <num1> <num2>")
+        sys.exit(1)
+ 
     num1 = float(sys.argv[1])
     num2 = float(sys.argv[2])
     
