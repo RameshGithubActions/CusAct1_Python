76d1c34c50a98f65b9e71c5a4c9045e9cffa9232
rameshkpit <151899670+rameshkpit@users.noreply.github.com>
2023-12-20 12:26:33 +0530

    Updated with function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 import sys
 
-result = 1
-n = len(sys.argv)
-for i in range(1, n):
-    result *= float(sys.argv[i])
 
-print("Result from Python File: " + result)    
+def multiplication_from_system_arguements():
+    result = 1
+    n = len(sys.argv)
+    for i in range(1, n):
+        result *= float(sys.argv[i])
+    return result
+
+
+if __name__ == "__main__":
+    print("Result from Python File: " + str(multiplication_from_system_arguements()))   
