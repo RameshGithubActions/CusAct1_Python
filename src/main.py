536c8c36add75438eece70ebe0e6c3bc0bd4ceaa
rameshkpit <151899670+rameshkpit@users.noreply.github.com>
2023-12-20 12:11:14 +0530

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,7 @@
-import sys
 import os
 
+result = 1
 n = len(sys.argv)
 for i in range(1, n):
-    print(sys.argv[i])
-result = 1
-for i in range(1, n):
     result *= float(sys.argv[i])
-print("Result:", result)
-os.environ["RESULT"] = str(result)
+    
